ddabaea37ffcb6da8b2ec4358af3dd3100614176
H. Vetinari <h.vetinari@gmx.com>
2020-04-26 20:14:32 +0200

    add check for system-libs in activation script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -22,5 +22,19 @@ rem if errorlevel 1 exit 1
 rem nmake -f ms\ntdll.mak
 rem if errorlevel 1 exit 1
 
+:: take care of activation scripts;
+:: from https://conda-forge.org/docs/maintainer/adding_pkgs.html#activate-scripts
+setlocal EnableDelayedExpansion
+
+:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+for %%F in (activate deactivate) DO (
+    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
+    copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+)
+:: TODO: add these in the loop above (comments in the loop can cause errors)
+:: Copy unix shell activation scripts, needed by Windows Bash users
+:: copy %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
+
 nmake test
 if errorlevel 1 exit 1
